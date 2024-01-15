9fcef6e8e7d5b17004b908a47c8721500086518d
vplauzon <vincent_lauzon@yahoo.com.au>
2024-01-15 15:18:27 -0500

    .

--- code/IntegrationTests/TestBase.cs
+++ code/IntegrationTests/TestBase.cs
@@ -1,10 +1,75 @@
 ﻿using System.Reflection;
+using System.Text.Json;
 
 namespace IntegrationTests
 {
     public abstract class TestBase
     {
+        #region Inner Types
+        private class MainSettings
+        {
+            public IDictionary<string, ProjectSetting>? Profiles { get; set; }
+
+            public IDictionary<string, string> GetEnvironmentVariables()
+            {
+                if (Profiles == null)
+                {
+                    throw new InvalidOperationException("'profiles' element isn't present in 'launchSettings.json'");
+                }
+                if (Profiles.Count == 0)
+                {
+                    throw new InvalidOperationException(
+                        "No profile is configured within 'profiles' element isn't present "
+                        + "in 'launchSettings.json'");
+                }
+                var profile = Profiles.First().Value;
+
+                if (profile.EnvironmentVariables == null)
+                {
+                    throw new InvalidOperationException("'environmentVariables' element isn't present in 'launchSettings.json'");
+                }
+
+                return profile.EnvironmentVariables;
+            }
+        }
+
+        private class ProjectSetting
+        {
+            public IDictionary<string, string>? EnvironmentVariables { get; set; }
+        }
+        #endregion
+
         private readonly string _testPath;
+
+        #region Static Construction
+        static TestBase()
+        {
+            FileToEnvironmentVariables();
+        }
+
+        private static void FileToEnvironmentVariables()
+        {
+            const string PATH = "Properties\\launchSettings.json";
+
+            if (File.Exists(PATH))
+            {
+                var settingContent = File.ReadAllText(PATH);
+                var mainSetting = JsonSerializer.Deserialize<MainSettings>(
+                    settingContent,
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    })
+                    ?? throw new InvalidOperationException("Can't read 'launchSettings.json'");
+                var variables = mainSetting.GetEnvironmentVariables();
+
+                foreach (var variable in variables)
+                {
+                    Environment.SetEnvironmentVariable(variable.Key, variable.Value);
+                }
+            }
+        }
+        #endregion
 
         protected TestBase(string testPath)
         {
