099e21c96f16c52d202812484f8d5181bdd7e6c4
vplauzon <vincent_lauzon@yahoo.com.au>
2024-01-15 17:01:29 -0500

    .

--- code/IntegrationTests/ExportManager.cs
+++ code/IntegrationTests/ExportManager.cs
@@ -1,5 +1,6 @@
 ﻿
 using Azure.Core;
+using Kusto.Cloud.Platform.Data;
 using Kusto.Data;
 using Kusto.Data.Common;
 using Kusto.Data.Net.Client;
@@ -10,6 +11,7 @@ namespace IntegrationTests
     {
         private readonly ICslAdminProvider _kustoProvider;
         private readonly string _database;
+        private readonly Lazy<Task<long>> _exportCapacityLazyInit;
 
         public ExportManager(
             Uri kustoIngestUri,
@@ -26,11 +28,26 @@ namespace IntegrationTests
 
             _kustoProvider = kustoProvider;
             _database = database;
+            _exportCapacityLazyInit = new Lazy<Task<long>>(
+                () => FetchCapacityAsync(),
+                LazyThreadSafetyMode.ExecutionAndPublication);
         }
 
         public async Task RunExportAsync(string script)
         {
-            await Task.CompletedTask;
+            var capacity = await _exportCapacityLazyInit.Value;
+        }
+
+        private async Task<long> FetchCapacityAsync()
+        {
+            using (var reader = await _kustoProvider.ExecuteControlCommandAsync(
+                _database,
+                ".show capacity data-export | project Total"))
+            {
+                var capacity = (long)reader.ToDataSet().Tables[0].Rows[0][0];
+
+                return capacity;
+            }
         }
     }
 }
