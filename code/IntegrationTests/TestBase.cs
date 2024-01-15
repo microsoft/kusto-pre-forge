005c5f8a5b73d142513575d4fddd9d85ef0f39da
vplauzon <vincent_lauzon@yahoo.com.au>
2024-01-15 15:02:51 -0500

    .

--- code/IntegrationTests/TestBase.cs
+++ code/IntegrationTests/TestBase.cs
@@ -4,16 +4,25 @@ namespace IntegrationTests
 {
     public abstract class TestBase
     {
-        protected async Task<string> GetEmbeddedContentAsync(string name)
+        private readonly string _testPath;
+
+        protected TestBase(string testPath)
+        {
+            _testPath = testPath;
+        }
+
+        protected async Task<string> GetEmbeddedScriptAsync()
         {
             var assembly = GetType().Assembly;
-            var fullName= $"{assembly.GetName().Name}.{name}";
+            string fullName = $"{assembly.GetName().Name}.{_testPath.Replace('/', '.')}.kql";
 
             using (var stream = assembly.GetManifestResourceStream(fullName))
             {
                 if (stream == null)
                 {
-                    throw new ArgumentException("No associated stream in assembly", nameof(name));
+                    throw new ArgumentException(
+                        "No associated stream in assembly",
+                        nameof(_testPath));
                 }
                 using (var reader = new StreamReader(stream))
                 {
--- code/IntegrationTests/Text/NoHeaderNoCompressionTest.cs
+++ code/IntegrationTests/Text/NoHeaderNoCompressionTest.cs
@@ -4,10 +4,15 @@ namespace IntegrationTests.Text
 {
     public class NoHeaderNoCompressionTest : TestBase
     {
+        public NoHeaderNoCompressionTest()
+            :base("Text/NoHeaderNoCompression")
+        {
+        }
+
         [Fact]
         public async Task TestOutput()
         {
-            var script = await GetEmbeddedContentAsync("Text.NoHeaderNoCompression.kql");
+            var script = await GetEmbeddedScriptAsync();
 
             await EnsureTemplateBlobTask(script);
         }
