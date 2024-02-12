1c1a571680911498bf54cf9acab451b96fe43647
vplauzon <vincent_lauzon@yahoo.com.au>
2024-02-12 16:10:06 -0500

    .

--- deployment/infra/deploy-infra.sh
+++ deployment/infra/deploy-infra.sh
@@ -28,4 +28,4 @@ echo "Deploying ARM template"
 az deployment group create -n "deploy-$(uuidgen)" -g $rg \
     --template-file main.bicep \
     --parameters testIdentityId=$testIdentityId testIdentityObjectId=$testIdentityObjectId \
-    '${main.parameters.json}'
+    '@{main.parameters.json}'
