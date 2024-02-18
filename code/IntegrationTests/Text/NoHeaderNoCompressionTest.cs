b92bd5ddc3caff078501ae9044df725012a0a2d8
vplauzon <vincent_lauzon@yahoo.com.au>
2024-02-17 19:46:11 -0500

    .

--- code/IntegrationTests/Text/NoHeaderNoCompressionTest.cs
+++ code/IntegrationTests/Text/NoHeaderNoCompressionTest.cs
@@ -1,5 +1,7 @@
 
 
+using System.Diagnostics.Metrics;
+
 namespace IntegrationTests.Text
 {
     public class NoHeaderNoCompressionTest : TestBase
@@ -14,29 +16,38 @@ namespace IntegrationTests.Text
         {
             await EnsureTableLoadAsync();
 
-            var counts = await QueryOneRowAsync(
-                $@"TextNoHeaderNoCompression
-| project Csv=parse_csv(Text)
-| project Id=tolong(Csv[0]), Timestamp=todatetime(Csv[1]), Level=tostring(Csv[2])
-| summarize MaxId=max(Id), RowCount=count()",
+            var result = await QueryOneRowAsync(
+                $@"
+let Data = TextNoHeaderNoCompression
+    | project Csv=parse_csv(Text)
+    | project Id=tolong(Csv[0]), Timestamp=todatetime(Csv[1]), Level=tostring(Csv[2]);
+let IdCardinality = toscalar(Data
+    | summarize by Id
+    | count);
+let TimestampCardinality = toscalar(Data
+    | summarize by Timestamp
+    | count);
+let LevelCardinality = toscalar(Data
+    | summarize by Level
+    | count);
+let RowCount = toscalar(Data
+    | count);
+print IdCardinality=IdCardinality,
+    TimestampCardinality=TimestampCardinality,
+    LevelCardinality=LevelCardinality,
+    RowCount=RowCount",
                 r => new
                 {
-                    MaxId = (long)r["MaxId"],
+                    IdCardinality = (long)r["IdCardinality"],
+                    TimestampCardinality = (long)r["TimestampCardinality"],
+                    LevelCardinality = (long)r["LevelCardinality"],
                     RowCount = (long)r["RowCount"]
                 });
 
-            Assert.NotNull(counts);
-            Assert.Equal(counts.MaxId, counts.RowCount);
-
-            var levels = await QueryAsync(
-                $@"TextNoHeaderNoCompression
-| project Csv=parse_csv(Text)
-| project Id=tolong(Csv[0]), Timestamp=todatetime(Csv[1]), Level=tostring(Csv[2])
-| summarize by Level",
-                r => (string)r["Level"]);
-
-            Assert.NotNull(levels);
-            Assert.Equal(3, levels.Count);
+            Assert.NotNull(result);
+            Assert.Equal(result.RowCount, result.IdCardinality);
+            Assert.Equal(result.RowCount, result.TimestampCardinality);
+            Assert.Equal(3, result.LevelCardinality);
         }
     }
 }
