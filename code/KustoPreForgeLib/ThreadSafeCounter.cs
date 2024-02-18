7a25a7304c700cbb29b387dce58cd0f3d355609d
vplauzon <vincent_lauzon@yahoo.com.au>
2024-02-18 15:19:52 -0500

    .

---
+++ code/KustoPreForgeLib/ThreadSafeCounter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KustoPreForgeLib
+{
+    internal class ThreadSafeCounter
+    {
+        private readonly TaskCompletionSource _taskSource = new();
+        private volatile int _counter = 0;
+
+        public Task BackToZeroTask => _taskSource.Task;
+
+        public void Increment()
+        {
+            Interlocked.Increment(ref _counter);
+        }
+
+        public void Decrement()
+        {
+            if (Interlocked.Decrement(ref _counter) == 0)
+            {
+                _taskSource.SetResult();
+            }
+        }
+    }
+}
