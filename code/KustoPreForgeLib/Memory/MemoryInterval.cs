9493ca32206b31899ba1c7316d292f54b188c197
vplauzon <vincent_lauzon@yahoo.com.au>
2024-04-30 18:15:17 -0400

    .

---
+++ code/KustoPreForgeLib/Memory/MemoryInterval.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KustoPreForgeLib.Memory
+{
+    internal struct MemoryInterval
+    {
+        public MemoryInterval(int offset, int length)
+        {
+            Offset = offset;
+            Length = length;
+        }
+
+        public int Offset { get; }
+
+        public int Length { get; }
+    }
+}
