c3d779ff1a937ef2557cf5674b2526561b232ad4
vplauzon <vincent_lauzon@yahoo.com.au>
2024-05-01 17:27:42 -0400

    .

--- code/KustoPreForgeLib/Transforms/DownloadBlobTransform.cs
+++ code/KustoPreForgeLib/Transforms/DownloadBlobTransform.cs
@@ -6,6 +6,8 @@ namespace KustoPreForgeLib.Transforms
 {
     internal class DownloadBlobTransform : IDataSource<BufferFragment>
     {
+        private const int MAX_READ_CONCURRENCY = 16;
+
         private readonly BufferFragment _buffer;
         private readonly IDataSource<BlobData> _blobSource;
         private readonly PerfCounterJournal _journal;
@@ -24,31 +26,58 @@ namespace KustoPreForgeLib.Transforms
             IAsyncEnumerable<SourceData<BufferFragment>>.GetAsyncEnumerator(
             CancellationToken cancellationToken)
         {
+            var workQueue = new WorkQueue<SourceData<BufferFragment>>();
+
             await foreach (var blobData in _blobSource)
             {
-                var blobBuffer = await _buffer.ReserveSubBufferAsync((int)blobData.Data.BlobSize);
-                var readOptions = new BlobOpenReadOptions(false)
+                if (workQueue.Count == MAX_READ_CONCURRENCY)
                 {
-                    BufferSize = blobBuffer.Length
-                };
-
-                using (var readStream = await blobData.Data.BlobClient.OpenReadAsync(readOptions))
-                {
-                    var size = await readStream.ReadAsync(blobBuffer.ToMemoryBlock());
-
-                    if (size != blobBuffer.Length)
-                    {
-                        throw new InvalidDataException("Mismatch in read size");
-                    }
+                    yield return await PumpContentOutAsync(workQueue);
                 }
-                //  To remove
-                blobBuffer.Release();
-                yield return new SourceData<BufferFragment>(
-                    blobBuffer,
-                    null,
-                    null,
-                    blobData);
+                workQueue.AddWorkItem(LoadBlobAsync(blobData));
             }
+            while (workQueue.Count > 0)
+            {
+                yield return await PumpContentOutAsync(workQueue);
+            }
+        }
+
+        private static async Task<SourceData<BufferFragment>> PumpContentOutAsync(
+            WorkQueue<SourceData<BufferFragment>> workQueue)
+        {
+            var data = await workQueue.WhenAnyAsync();
+
+            //  To remove
+            data.Data.Release();
+            
+            return data;
+        }
+
+        private async Task<SourceData<BufferFragment>> LoadBlobAsync(
+            SourceData<BlobData> blobData)
+        {
+            var blobBuffer = await _buffer.ReserveSubBufferAsync((int)blobData.Data.BlobSize);
+            var readOptions = new BlobOpenReadOptions(false)
+            {
+                BufferSize = blobBuffer.Length
+            };
+
+            using (var readStream = await blobData.Data.BlobClient.OpenReadAsync(readOptions))
+            {
+                var size = await readStream.ReadAsync(blobBuffer.ToMemoryBlock());
+
+                if (size != blobBuffer.Length)
+                {
+                    throw new InvalidDataException("Mismatch in read size");
+                }
+            }
+            _journal.AddReading("DownloadBlob.BlobRead", 1);
+
+            return new SourceData<BufferFragment>(
+                blobBuffer,
+                null,
+                null,
+                blobData);
         }
     }
 }
--- code/KustoPreForgeLib/WorkQueue.cs
+++ code/KustoPreForgeLib/WorkQueue.cs
@@ -1,25 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace KustoPreForgeLib
 {
+    /// <summary>
+    /// Component keeping track of multiple asynchronous tasks.
+    /// This isn't multithreaded and is assumed to be used one method call at the time.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     internal class WorkQueue<T>
     {
-        public int Size => throw new NotImplementedException();
+        private readonly List<Task<T>> _queue = new();
+
+        public int Count => _queue.Count;
 
         public void AddWorkItem(Task<T> task)
         {
-            throw new NotImplementedException();
+            _queue.Add(task);
         }
 
-        public async Task<T> WhenAnyAsync(Task task)
+        public async Task<T> WhenAnyAsync()
         {
-            await Task.CompletedTask;
+            await Task.WhenAny(_queue);
 
-            throw new NotImplementedException();
+            for (int i = 0; i != _queue.Count; i++)
+            {
+                if (_queue[i].IsCompleted)
+                {
+                    var value = await _queue[i];
+
+                    _queue.RemoveAt(i);
+
+                    return value;
+                }
+            }
+
+            throw new InvalidOperationException("Can't find completed task");
         }
     }
 }
