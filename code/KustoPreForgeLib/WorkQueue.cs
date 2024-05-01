b4532e30b6431b0d4e69a0bdbb4d06e3f41f14f7
vplauzon <vincent_lauzon@yahoo.com.au>
2024-05-01 16:54:28 -0400

    .

---
+++ code/KustoPreForgeLib/WorkQueue.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KustoPreForgeLib
+{
+    internal class WorkQueue<T>
+    {
+        public int Size => throw new NotImplementedException();
+
+        public void AddWorkItem(Task<T> task)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<T> WhenAnyAsync(Task task)
+        {
+            await Task.CompletedTask;
+
+            throw new NotImplementedException();
+        }
+    }
+}
