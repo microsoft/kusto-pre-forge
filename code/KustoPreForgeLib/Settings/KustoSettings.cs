9994ed44a1c67e529e9bcd720ad7b60975e0efa6
vplauzon <vincent_lauzon@yahoo.com.au>
2024-05-02 20:19:40 -0400

    .

---
+++ code/KustoPreForgeLib/Settings/KustoSettings.cs
@@ -0,0 +1,28 @@
+﻿namespace KustoPreForgeLib.Settings
+{
+    public class KustoSettings
+    {
+        public KustoSettings(
+            Uri? kustoIngestUri,
+            string? kustoDb,
+            string? kustoTable)
+        {
+            IngestUri = kustoIngestUri;
+            Database = kustoDb;
+            Table = kustoTable;
+        }
+
+        public Uri? IngestUri { get; }
+
+        public string? Database { get; }
+
+        public string? Table { get; }
+
+        public void WriteOutSettings()
+        {
+            Console.WriteLine($"KustoIngestUri:  {IngestUri}");
+            Console.WriteLine($"KustoDb:  {Database}");
+            Console.WriteLine($"KustoTable:  {Table}");
+        }
+    }
+}
