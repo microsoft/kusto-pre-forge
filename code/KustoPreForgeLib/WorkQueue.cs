684d586a6d74dc1c3c9e27c4826c702e90ce0fc3
vplauzon <vincent_lauzon@yahoo.com.au>
2024-05-13 13:44:49 -0400

    .

--- code/KustoPreForgeLib/WorkQueue.cs
+++ code/KustoPreForgeLib/WorkQueue.cs
@@ -144,6 +144,8 @@ namespace KustoPreForgeLib
             {
                 var task = ExecuteTaskAsync(asyncFunction);
 
+                _runningTasks.Enqueue(task);
+
                 return;
             }
 
